65738e12f3be72556b9fcc0339c726aa7207bbcc
jhos-vac <103053087+jhos-vac@users.noreply.github.com>
2023-05-30 09:17:07 -0500

    Initial commit

---
+++ src/main/kotlin/com/example/proyecto_tercer_semestre_backend/ProyectoTercerSemestreBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.example.proyecto_tercer_semestre_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProyectoTercerSemestreBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProyectoTercerSemestreBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/proyecto_tercer_semestre_backend/ProyectoTercerSemestreBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.proyecto_tercer_semestre_backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProyectoTercerSemestreBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
